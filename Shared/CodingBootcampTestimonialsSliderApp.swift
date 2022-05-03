7c762e243b1a9c86871c4be1dd53f9f3b572cf37
Jinwook Kim <redwoodnatlpark@gmail.com>
2022-05-03 09:39:20 +0900

    Initial Commit

---
+++ Shared/CodingBootcampTestimonialsSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingBootcampTestimonialsSliderApp.swift
+//  Shared
+//
+//  Created by Jinwook Kim on 2022/05/03.
+//
+
+import SwiftUI
+
+@main
+struct CodingBootcampTestimonialsSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
